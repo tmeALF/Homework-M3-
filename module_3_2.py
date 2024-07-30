50a1ae4d0a06c9e67d4794d39ceb3ec80fef504b
tmeALF <m1160955@yandex.ru>
2024-07-30 16:14:10 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,18 +1,18 @@
 def send_email(message, recipient, sender='university.help@gmail.com'):
+
     if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
         return
-
-    if ("@" or (".com" or ".ru" or ".net")) in (recipient and sender):
-
-        if sender == 'university.help@gmail.com':
-            print("Письмо успешно отправлено с адреса ", sender, " на адрес ", recipient)
-        else:
-            print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, " на адрес ", recipient, ".")
-    else:
+    if sender == 'university.help@gmail.com':
+        print("Письмо успешно отправлено с адреса ", sender, " на адрес ", recipient)
+        return
+    if ("@" and (".com" or ".ru" or ".net")) not in (recipient and sender):
         print("Невозможно отправить письмо с адреса ,", sender, "  на адрес ", recipient)
+    elif sender != 'university.help@gmail.com':
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, " на адрес ", recipient, ".")
 
-# send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-# send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-# send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-# send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
